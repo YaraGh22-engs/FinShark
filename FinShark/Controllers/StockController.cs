7acd6981a3d36472278692f7cec68ac6bde7e6c4
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-06-19 18:19:40 +0300

    Controllers

---
+++ FinShark/Controllers/StockController.cs
@@ -0,0 +1,30 @@
+﻿using FinShark.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stock = _context.Stocks.ToList();
+            return Ok(stock);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null) return NotFound();
+            return Ok(stock);
+        }
+    }
+}
